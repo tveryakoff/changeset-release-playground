c15b9d9a9d792a5cc4a8d00e52b81971bc5eceff
tveryakoff <tveryakov@icloud.com>
2024-05-07 16:24:57 +0100

    5 version bump

--- packages/my-package/index.js
+++ packages/my-package/index.js
@@ -1,5 +1,1 @@
-console.log('Hello world!!!!!!')
-console.log('major changese')
-console.log('major changes after alfa 4')
-console.log('major changes after alfa 5')
-console.log('major changes after alfa 6')
+console.log('5')
